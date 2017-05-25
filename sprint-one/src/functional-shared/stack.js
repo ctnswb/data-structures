b2ec0fa4d555d1d0ea640505011d1826b5fa0a48
ctnswb <xclai.07@gmail.com>
2017-05-24 21:31:56 -0700

    functional queue and functional-shared stack implemented

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.stackSize = 0;
+  newStack.storage = {};
+
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+
+stackMethods.size = function(){
+  return this.stackSize;
+};
+
+stackMethods.push = function(value){
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+}
+
+stackMethods.pop = function(){
+  if (this.stackSize > 0) {
+    var deletedValue = this.storage[this.stackSize - 1];
+    delete deletedValue;
+    this.stackSize--;
+    return deletedValue;
+  }
+
+}
 
