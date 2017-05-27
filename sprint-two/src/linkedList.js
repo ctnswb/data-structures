d9aaa7873cdfc084f8b82f19cb9c31fd03615fad
ctnswb <xclai.07@gmail.com>
2017-05-27 12:16:43 -0700

    implement linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var formerHead = list.head;
+      list.head = formerHead.next;
+      return formerHead.value;
+    }
   };
 
   list.contains = function(target) {
+    var found = false;
+    var checkNodeValue = function(node) {
+      if (!found) {
+        if (node.value === target) {
+          found = true;
+        } else {
+          if (node.next !== null) {
+            checkNodeValue(node.next);
+          }
+        }
+      }
+    };
+    checkNodeValue(list.head);
+    return found;
   };
 
   return list;
@@ -26,4 +53,8 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ addToTail: O(1)
+ removeHead: O(1)
+ contains: O(n)
  */
