652ed41ebc17ceaf96d7cc5a5c2fb69b04a02f8e
ctnswb <xclai.07@gmail.com>
2017-05-29 20:56:56 -0700

    implement binary search tree. partial implementation of hash table

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,55 @@
 var BinarySearchTree = function(value) {
-};
+  var bst = {};
+  bst.value = value;
+  bst.left = null; // a tree
+  bst.right = null;
+
+  bst.insert = function(value) {
+   if (value < bst.value) {
+      if (bst.left !== null) {
+        bst.left.insert(value);
+      } else {
+         bst.left = BinarySearchTree(value);
+      }
+   } else if (value > bst.value) {
+      if (bst.right !== null) {
+        bst.right.insert(value);
+      } else {
+        bst.right = BinarySearchTree(value);
+      }
+   }
+  };
+
+  bst.contains = function(value) {
+    if (bst.value === value) {
+      return true;
+    } else if (bst.value < value && bst.right !== null) {
+      return bst.right.contains(value);
+    } else if (bst.value > value && bst.left !== null) {
+      return bst.left.contains(value);
+    } else {
+      return false;
+    }
+  };
+
+  bst.depthFirstLog = function(callback) {
+    callback(bst.value);
+
+    if (bst.left !== null) {
+      bst.left.depthFirstLog(callback);
+    }
+    if (bst.right !== null) {
+      bst.right.depthFirstLog(callback);
+    }
+  };
+
+  return bst;
+}
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+insert: O(log n)
+contains: O(log n)
+depthFirstLog: O(n)
  */
